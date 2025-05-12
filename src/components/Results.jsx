1e36fb52cd965284a38e59aaa0f15eef673d9ffc
it22587206 <it22587206@my.sliit.lk>
2025-05-12 11:13:58 +0530

    create the result page

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,46 @@
-import { calculateInvestmentResults } from '../util/investment.js';
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
-export default function Results ( {inputs} ) {
+export default function Results({ input }) {
+  const { initialInvesment, annuelInvesment, expectedReturn, duration } = input;
 
-    const resultData = calculateInvestmentResults(inputs);
-    console.log(resultData);
+  // Map the input object to the expected parameter structure
+  const resultData = calculateInvestmentResults({
+    initialInvestment: initialInvesment,
+    annualInvestment: annuelInvesment,
+    expectedReturn: expectedReturn,
+    duration: duration,
+  });
 
-    return <p>Results...</p>
-    
+  // Rename the local variable to avoid conflicts
+  const baseInvestment = resultData[0].valueEndOfYear - resultData[0].interest - resultData[0].annualInvestment;
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - baseInvestment;
+          const totalInvested = yearData.valueEndOfYear - totalInterest;
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
